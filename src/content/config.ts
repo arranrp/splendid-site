8ae1a1da8e654f3024205905bbce68fbe8af3ea3
Maciej Pędzich <contact@maciejpedzi.ch>
2023-06-05 16:38:15 +0200

    Rename blog collection to events and add speakers

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,38 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
+const events = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    socialDescription: z.string(),
+    socialImage: z.string().optional(),
+    // Transform string to Date object
+    startDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    endDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    tags: z.string().array(),
+    signupLink: z.string().optional()
+  })
 });
 
-export const collections = { blog };
+const speakers = defineCollection({
+  schema: z.object({
+    name: z.string(),
+    avatar: z.string(),
+    largeImage: z.string().optional(),
+    socialLinks: z.object({
+      github: z.string().optional(),
+      twitter: z.string().optional(),
+      mastodon: z.string().optional(),
+      personalWebsite: z.string().optional(),
+      companyWebsite: z.string().optional()
+    })
+  })
+});
+
+export const collections = { events, speakers };
